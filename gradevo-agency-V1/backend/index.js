64777b6e11013b4e2853438395518540b55f0aaf
Krishna Murthi <krishnamurthikm07@gmail.com>
2025-11-25 16:23:44 +0530

    chore: remove local uploads folder and static serving

--- gradevo-agency-V1/backend/index.js
+++ gradevo-agency-V1/backend/index.js
@@ -38,7 +38,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(express.json());
-app.use('/uploads', express.static(join(__dirname, 'uploads')));
+
 
 // Routes
 app.use('/api/auth', authRoutes);
